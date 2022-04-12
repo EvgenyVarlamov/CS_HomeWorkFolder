6ebaf43ba646122ff108a204e2c79090097119de
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-04-12 22:59:26 +0300

    add Task 009 Homework #5 Even numbers from 1 to N

---
+++ Task_009/Program.cs
@@ -0,0 +1,19 @@
+﻿//  ДЗ: программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число больше 1");
+int numberN = Convert.ToInt32(Console.ReadLine());   // Ввод числа
+int numberCount = 0;                                 // Переменная для работы цикла
+
+if(numberN > 1)                                      // Проверка введенного числа
+  {
+   Console.Write("Чётные числа: ");
+   while(numberCount < numberN)                      // Перебор от нуля до введенного числа
+   {
+      numberCount++;
+      if(numberCount%2 == 0)
+         {
+          Console.Write(numberCount + ", ");         // Вывод чётных чисел
+         }
+   }
+  }
+else Console.Write("Вы ввели некорректное число");   // Вывод сообщения о некорректно введенном числе
